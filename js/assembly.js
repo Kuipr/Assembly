7b3a8f3a9bfebecbfce494dc67d19d41454c0e04
Mark Adkins <mark@bymark.co>
2021-01-10 19:28:10 -0500

    Initial Commit

--- js/assembly.js
+++ js/assembly.js
@@ -1,9 +1,18 @@
+/*!
+ * Assembly Framework (https://kuipr.io/assembly/)
+ * Copyright (c) 2018 Kuipr, LLC. (https://github.com/kuipr)
+ * Copyright (c) 2018 Mark Adkins (https://github.com/funkybunch)
+ * Licensed under MIT (https://github.com/kuipr/assembly/LICENSE)
+ */
+
 'use strict';
 
 /**
  * Sends console.log() message with current status of build.
  */
+
+import { version } from '../package.json';
+
 module.exports = function() {
-    console.log("Assembly is a CSS only framework at this time.  This will change prior to the v1.0.0 release but we are getting our styles together first.")
     return true;
 };
--- test/test.js
+++ test/test.js
@@ -4,8 +4,20 @@ var expect = require('chai').expect;
 var installCheck = require('../dist/js/assembly.min.js');
 
 describe('#installCheck', function() {
-    it('should indicate a successful install', function() {
+    it('Should indicate a successful install', function() {
         var result = installCheck();
+        expect(result).to.equal(true);
+    });
+});
+
+describe('#installCheck', function() {
+    it('Should show a Tooltip object is created', function() {
+        var tooltip     = new Tooltip.Tooltip();
+        var result      = false;
+        console.log(tooltip);
+        if(tooltip !== null && typeof tooltip === 'object') {
+            result = true;
+        }
         expect(result).to.equal(true);
     });
 });
